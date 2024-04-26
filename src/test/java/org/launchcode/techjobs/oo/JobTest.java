60df7e6f02c61497f41d66708a1fe2400e19f7aa
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-04-26 13:54:22 -0500

    task 4

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,39 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 public class JobTest {
-    //TODO: Create your unit tests here
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        // Declare and initialize a new Job object
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        // Test that each field is assigned the correct class
+        assertTrue(testJob.getName() instanceof String);
+        assertTrue(testJob.getEmployer() instanceof Employer);
+        assertTrue(testJob.getLocation() instanceof Location);
+        assertTrue(testJob.getPositionType() instanceof PositionType);
+        assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+
+        // Test that each field is assigned the correct value
+        assertEquals("Product tester", testJob.getName());
+        assertEquals("ACME", testJob.getEmployer().getValue());
+        assertEquals("Desert", testJob.getLocation().getValue());
+        assertEquals("Quality control", testJob.getPositionType().getValue());
+        assertEquals("Persistence", testJob.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        // Generate two Job objects that have identical field values except for id
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        // Test that equals returns false
+        assertFalse(job1.equals(job2));
+    }
 }
